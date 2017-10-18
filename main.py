029a3816e5fecd5624b428819a0cf02755374bf2
Santos, Marcel S <=>
2017-10-18 10:13:42 -0300

    Some minor adjusts

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 PATH = "./datasets/mammography-consolidated.csv"
 
+def split_datatset(dataset):
+	X = dataset.iloc[:, :-1].values
+	y = dataset.iloc[:, -1].values
+
+	X_train, X_test, y_train, y_test = \
+		train_test_split(X, y, test_size=1/4, random_state=42, stratify=y)
+
+	X_train, X_val, y_train, y_val = \
+		train_test_split(X_train, y_train, test_size=1/3, random_state=42, stratify=y_train)
+
+	return {'X_train' : X_train, 
+			'y_train' : y_train, 
+			'X_test' : X_test, 
+			'y_test' : y_test, 
+			'X_val' : X_val, 
+			'y_val' : y_val}
+
+#def 
 
 def main():
 	headers = ["f1", "f2","f3", "f4","f5", "f6","target"]
@@ -13,28 +31,15 @@ def main():
 	no_cancer = dataset[dataset["target"]==0]
 	has_cancer = dataset[dataset["target"]==1]
 
+	# ToDO: make it more generic
 	has_cancer = has_cancer.append([has_cancer]*29, ignore_index=True)
 	has_cancer = has_cancer.drop(has_cancer.index[0:25])
 
-	X_has_cancer = has_cancer.iloc[:, :-1].values
-	y_has_cancer = has_cancer.iloc[:, -1].values
-	## Treino: 50%, Validação: 25%, Teste: 25%
-	X_has_cancer_train, X_has_cancer_test, y_has_cancer_train, y_has_cancer_test = \
-	 train_test_split(X_has_cancer, y_has_cancer, test_size=1/4, random_state=42, stratify=y_has_cancer)
+	# Split dataset 
+	no_cancer_splitted = split_datatset(no_cancer)
+	has_cancer_splitted = split_datatset(has_cancer)
 
-	X_has_cancer_train, X_has_cancer_val, y_has_cancer_train, y_has_cancer_val = \
-	 train_test_split(X_has_cancer_train, y_has_cancer_train, test_size=1/3, random_state=42, stratify=y_has_cancer_train)
-
-
-	X_no_cancer = has_cancer.iloc[:, :-1].values
-	y_no_cancer = has_cancer.iloc[:, -1].values
-	## Treino: 50%, Validação: 25%, Teste: 25%
-	X_no_cancer_train, X_no_cancer_test, y_no_cancer_train, y_no_cancer_test = \
-	 train_test_split(X_no_cancer, y_no_cancer, test_size=1/4, random_state=42, stratify=y_no_cancer)
-	 
-	X_no_cancer_train, X_has_cancer_val, y_no_cancer_train, y_no_cancer_val = \
-	 train_test_split(X_no_cancer_train, y_no_cancer_train, test_size=1/3, random_state=42, stratify=y_no_cancer_train)
-
+	# merge dataframes
 
 	return
 
