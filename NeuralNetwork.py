75c131d32e59154d8bbd0cf24cad20db2d9cf236
Santos, Marcel S <=>
2017-11-16 22:28:24 -0300

    I know that is not correct use a message which does not describe well the commit propose but i am so tired today and it is too late, so I will push my code to master and I hope everything will be fine

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,8 +2,10 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.callbacks import EarlyStopping
-from Util import plot_training_error_curves
-from keras.regularizers import l2
+from Util import plot_training_error_curves, extract_final_losses
+from keras.regularizers import l2, l1
+
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score
 import numpy as np
 
 class NeuralNetworkGenerator:
@@ -11,7 +13,7 @@ class NeuralNetworkGenerator:
 	architectures = []
 	architectures_scores = []
 
-	def __init__(self, path, epochs = 150, batch_size = 50):
+	def __init__(self, path, epochs = 150, batch_size = 64):
 		self.path = path
 		self.epochs = epochs
 		self.batch_size =  batch_size
@@ -27,13 +29,10 @@ class NeuralNetworkGenerator:
 				                           })
 
 	def evaluate(self, dataset):
-		print("Evaluating achitectures: \n")
-		print("batch_size: %d epochs: %d" %(self.batch_size, self.epochs))
-
-		np.random.seed(7)
 		x = 1
+		np.random.seed(7)
+
 		for arch in self.architectures:
-			# Create model.
 			model = Sequential()
 
 			for i in range(len(arch['layers'])):
@@ -42,44 +41,46 @@ class NeuralNetworkGenerator:
 					i += 1
 				elif i < len(arch['layers']) - 1:
 					
-					# if arch['regularization'] > 0:
-						#model.add(Dropout(0.2))
-						model.add(Dense(arch['layers'][i], kernel_initializer='random_normal',  activation = arch['activation'], 
-									activity_regularizer=l2(arch['regularization'])))
-					# else:
-					# 	model.add(Dense(arch['layers'][i], activation = arch['activation']))
+					model.add(Dense(arch['layers'][i], activation=arch['activation'], activity_regularizer=l2(arch['regularization'])))
 
 				else:
-					#model.add(Dropout(0.2))
 					model.add(Dense(arch['layers'][i], activation = 'sigmoid'))
 
 			# Compile model.
-			model.compile(loss = 'mean_squared_error', optimizer = 'adam', metrics = ['accuracy'])
+			model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
-			# Get dataset variables
+			# Get dataset variables.
 			dset = dataset.get_dataset()
 
 			# Fit the model.
 			history = model.fit(dset['X_train'], dset['y_train'], epochs = self.epochs, batch_size = self.batch_size,
-			                    callbacks = [EarlyStopping(monitor='val_loss', patience = 10)], validation_data = (dset['X_val'], dset['y_val']))
+			                    callbacks = [EarlyStopping(patience = 50)], validation_data = (dset['X_val'], dset['y_val']))
 
 			# Evaluate the model.
+			#scores = model.evaluate(dset['X_test'], dset['y_test'])
+			#self.architectures_scores.append((scores, len(history.history['loss'])))
 
-			scores = model.evaluate(dset['X_test'], dset['y_test'])
-			self.architectures_scores.append((scores, len(history.history['loss'])))
-			plot_training_error_curves(history, x)
+			y_pred = model.predict(dset['X_test'])
+			y_pred_class = model.predict_classes(dset['X_test'], verbose = 0)
 
-			print("Architecture %d: " %x)
-			print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-			print("--------------------------------------------------------------")
+			plot_training_error_curves(history)
+			losses = extract_final_losses(history)
+			print()
+			print("{metric:<18}{value:.4f}".format(metric = "Train Loss:", value = losses['train_loss']))
+			print("{metric:<18}{value:.4f}".format(metric = "Validation Loss:", value = losses['val_loss']))
+			print("{metric:<18}{value:.4f}".format(metric = "Accuracy:", value = accuracy_score(dset['y_test'], y_pred_class)))
+			print("{metric:<18}{value:.4f}".format(metric = "Recall:", value = recall_score(dset['y_test'], y_pred_class)))
+			print("{metric:<18}{value:.4f}".format(metric = "Precision:", value = precision_score(dset['y_test'], y_pred_class)))
+			print("{metric:<18}{value:.4f}".format(metric = "F1:", value = f1_score(dset['y_test'], y_pred_class)))
+			print("{metric:<18}{value:.4f}".format(metric = "AUROC:", value = roc_auc_score(dset['y_test'], y_pred)))
+
 			x += 1
 
-		##self.store_test_scores()
+		#self.store_test_scores()
 
 	def store_test_scores(self):
-		print("Storing test scores...")
-
 		i = 1
+		print("Storing test scores...")
 		with open("results/output.txt", "w") as f:
 			for scores in self.architectures_scores:
 				f.write("\nArchitecture %d \n%s: %.2f%% (%d)" % (i, "accuracy", scores[0][1] * 100, scores[1]))
